35c3994e49d3a7912d02ea3d905e9be3ea8b4594
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-21 12:49:01 +0200

    added solved first 5 tasks

--- tasks.js
+++ tasks.js
@@ -1,7 +1,43 @@
 import users from './users.js';
 
+//task-01
 
-
-const getUserNames = users.map(user => user.name);
-console.log(getUserNames);
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
+console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+//task-02
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color);
+};
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+
+//task-03
+
+const getUsersWithGender = (users, gender) => {
+  return users.filter(user => user.gender === gender);
+};
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+
+//task-04
+const getInactiveUsers = users => {
+ return users.filter(user => !user.isActive)
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+//task-05
+const getUserWithEmail = (users, email) => {
+  return users.find(user => user.email === email)
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+//task-06

41b7606b25a6284e0cb3d292f4e8b05120cecc2d
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-21 11:52:00 +0200

    solved first task

---
+++ tasks.js
@@ -0,0 +1,7 @@
+import users from './users.js';
+
+
+
+const getUserNames = users.map(user => user.name);
+console.log(getUserNames);
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]

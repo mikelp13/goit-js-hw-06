2360c1830fc5db31e0914507b306a3f7e94d6888
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-11-21 23:23:56 +0200

    added solved task-09

--- tasks.js
+++ tasks.js
@@ -58,21 +58,31 @@ console.log(calculateTotalBalance(users)); // 20916
 */
 //task-08
 
-const getUsersWithFriend = (users, friendName) => {
- const friendsArray = users.reduce((names, user) => { 
-       user.friends.forEach(friend =>{
-        if(friend === friendName){
-          names.push(user.name);
-            }
-       });
-       return names; 
- }, []);
+// const getUsersWithFriend = (users, friendName) => {
+//  const friendsArray = users.reduce((names, user) => { 
+//        user.friends.forEach(friend =>{
+//         if(friend === friendName){
+//           names.push(user.name);
+//             }
+//        });
+//        return names; 
+//  }, []);
 
-return friendsArray;
+// return friendsArray;
 
-};
+// };
 
- console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
- console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+//  console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+//  console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 //task-09
+
+const getNamesSortedByFriendsCount = users => {
+  const usersCopy = [...users];
+  usersCopy.sort((a, b) => a.friends.length - b.friends.length);
+  
+ return usersCopy.map(user => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
